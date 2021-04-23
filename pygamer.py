cf046ba64a6728da3188d7c9b5f26f536dd8b52b
Akshat <80572104+akshat-rana740@users.noreply.github.com>
2021-04-23 15:39:15 +0530

    Update pygamer.py

--- pygamer.py
+++ pygamer.py
@@ -1,1 +1,15 @@
 #this is a game using pygame
+import pygame
+#this is your code for creating window and adding title to it
+GameScreen = pygame.display.set_mode((1200,900))
+pygame.display.set_caption("Game")
+#this is the code for continous losding of screen
+exit_game = False
+while not exit_game:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      exit_game=True
+pygame.quit()
+quit()
+
+      
